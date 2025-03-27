2c568213ea1101fe267382ed512d84d2df56edb7
m.muehlbauer <muehlbauer@cubert-gmbh.de>
2025-03-27 08:21:54 +0100

    fixed indentation

--- EfficientAD/ImageNet_6ch_generator.py
+++ EfficientAD/ImageNet_6ch_generator.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2 as cv
 from tqdm import tqdm
 
-datasets = [["..\\data\\ImageNet", ".JPEG"]] # enter path Imagenet dataset here
+datasets = [["../data/ImageNet", ".JPEG"]]  # enter path to Imagenet dataset here
 
 for dataset in datasets:
     path = Path(dataset[0])
@@ -12,15 +12,14 @@ for dataset in datasets:
 
     print("found", len(files), "files in", dataset, "ext:", ext, "path:", path, "exists", path.exists())
 output_dir = path.parent / (path.name + "_6_channel")
-    for file in tqdm(files):
+for file in tqdm(files):
+    img = cv.imread(str(file))
+    extended_img = np.zeros((img.shape[0], img.shape[1], 6), dtype='uint8')
+    extended_img[:, :, :3] = img
+    extended_img[:, :, 3:] = img
 
-        img = cv.imread(str(file))
-        extended_img = np.zeros((img.shape[0], img.shape[1], 6), dtype='uint8')
-        extended_img[:, :, :3] = img
-        extended_img[:, :, 3:] = img
+    relative = file.relative_to(path)
+    new_path = output_dir / relative.with_suffix('.npy')
+    new_path.parent.mkdir(parents=True, exist_ok=True)
 
-        relative = file.relative_to(path)
-        new_path = output_dir / relative.with_suffix('.npy')
-        new_path.parent.mkdir(parents=True, exist_ok=True)
-
-        np.save(new_path, extended_img)
+    np.save(new_path, extended_img)
